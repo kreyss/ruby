74a96d53537e50f54acbdfd41c989d1e48723eed
kreyss <61807819+kreyss@users.noreply.github.com>
2020-03-06 22:24:52 +0200

    lesson 1

--- lesson1/work3.rb
+++ lesson1/work3.rb
@@ -7,31 +7,28 @@ side_b = gets.chomp.to_i
 puts "Введите сторону c"
 side_c = gets.chomp.to_i
 
-
 #находит гипатенузу
-if (side_a>side_b)&&(side_a>side_c)
-	cathet1 = side_b
-	cathet2 = side_c
-	hypotenuse = side_a
-	elsif (side_b>side_a)&&(side_b>side_c)
-		cathet1 = side_a
-		cathet2 = side_c
-		hypotenuse = side_b
-	else
-		cathet1 = side_a
-		cathet2 = side_b
-		hypotenuse = side_c
-		
-	end
-
+if (side_a > side_b) && (side_a > side_c)
+  cathet1 = side_b
+  cathet2 = side_c
+  hypotenuse = side_a
+elsif (side_b > side_a) && (side_b > side_c)
+  cathet1 = side_a
+  cathet2 = side_c
+  hypotenuse = side_b
+else
+  cathet1 = side_a
+  cathet2 = side_b
+  hypotenuse = side_c
+end
 #находит треугольник
 if (hypotenuse**2) == (cathet1**2) + (cathet2**2)
  puts "Это правельный треугольник"
 
-elsif (side_a == side_b)&&(side_b != side_c)||(side_b == side_c)&&(side_a != side_c)||(side_a == side_c)&&(side_b != side_c)
- 	puts "Это равнобедренный треугольник"
- elsif (side_a==side_b)&&(side_b==side_c)&&(side_a==side_c)
- 	puts "Равнобедренный равностороний треугольник"
+elsif (side_a == side_b) && (side_b != side_c) || (side_b == side_c) && (side_a != side_c) || (side_a == side_c) && (side_b != side_c)
+  puts "Это равнобедренный треугольник"
+elsif (side_a == side_b) && (side_b == side_c) && (side_a == side_c)
+  puts "Равнобедренный равностороний треугольник"
 else
-	puts "это треугольник с разными сторонами "
+  puts "это треугольник с разными сторонами "
 end
--- lesson1/work4.rb
+++ lesson1/work4.rb
@@ -7,19 +7,15 @@ b = gets.chop.to_i
 puts "третий коэффициент"
 c = gets.chop.to_i
 
-	discriminant = b**2 - 4 * a * c
+discriminant.to_f = Math.sqrt(b ** 2 - 4 * a * c)
 
 if discriminant > 0 
-	x1 = (-b + Math.sqrt(discriminant))/ 2*a
-	x2 = (-b - Math.sqrt(discriminant))/ 2*a
-	puts "дискриминант имеет два корня и это #{x1} , #{x2}"
-
+  x1.to_f = (-b + discriminant) / 2 * a
+  x2.to_f = (-b - discriminant) / 2 * a
+  puts "дискриминант имеет два корня и это #{x1} , #{x2}"
 elsif discriminant == 0 
-	x1 = -b / 2*a
-	puts "дискриминант имеет один корень и это #{x1}"
-
+  x1.to_f = -b / 2 * a
+  puts "дискриминант имеет один корень и это #{x1}"
 else 
-puts "дискриминант не имеет корней"
-
-
+  puts "дискриминант не имеет корней"
 end
