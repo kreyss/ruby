0c241d5acf3e490f59ab516402d62e8d22cc737b
kreyss <61807819+kreyss@users.noreply.github.com>
2020-03-06 14:22:29 +0200

    Update work3(2).rb

--- lesson1/work3(2).rb
+++ lesson1/work3(2).rb
@@ -12,20 +12,20 @@ c=a**2+b**2
 c=Math.sqrt(c)
 
 if (a>b)&&(a>b)
-	cathetus1 = b
-	cathetus2 = c
+	cathet1 = b
+	cathet2 = c
 	hypotenuse =  a
 	elsif (b>a)&&(b>c)
-		cathetus1 = a
-		cathetus2 = c
+		cathet1 = a
+		cathet2 = c
 		hypotenuse = b
 	else
-		cathetus1 = a
-		cathetus2 = b
+		cathet1 = a
+		cathet2 = b
 		hypotenuse = c
 		
 	end
-if (hypotenuse**2) == (cathetus1**2) + (cathetus2**2)
+if (hypotenuse**2) == (cathet1**2) + (cathet2**2)
 	p "треугольник прямоугольным"
 elsif a.to_f==b.to_f && a.to_f==c.to_f && c.to_f==b.to_f
   puts "треугольник равностороний"
