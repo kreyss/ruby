17cca928c480a140bc5a69141b0bedf2bbdcbb45
kreyss <61807819+kreyss@users.noreply.github.com>
2020-03-07 16:48:57 +0200

    lesson 1

--- lesson1/work4.rb
+++ lesson1/work4.rb
@@ -10,8 +10,8 @@ c = gets.chop.to_i
 discriminant.to_f = Math.sqrt(b ** 2 - 4 * a * c)
 
 if discriminant > 0 
-  x1.to_f = (-b + discriminant) / 2 * a
-  x2.to_f = (-b - discriminant) / 2 * a
+  x1 = (-b + discriminant) / 2 * a
+  x2 = (-b - discriminant) / 2 * a
   puts "дискриминант имеет два корня и это #{x1} , #{x2}"
 elsif discriminant == 0 
   x1.to_f = -b / 2 * a
