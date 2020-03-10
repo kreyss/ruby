ab4d6e48e2c5fa06068c8d7f8e449b850c2319a0
kreyss <61807819+kreyss@users.noreply.github.com>
2020-03-10 18:22:31 +0200

    lesson 2

--- lesson2/work4.rb
+++ lesson2/work4.rb
@@ -1,6 +1,6 @@
 vowels = ["а", "у", "о", "ы", "и", "э", "я", "ю", "ё", "е"]
 vowel_hash = {}
 
-("а".."я").each_with_index do |later, index| vowel_hash[later] = index if vowels.include?(later)
+("а".."я").each.with_index(1) do |later, index| vowel_hash[later] = index if vowels.include?(later)
 end
  print "#{vowel_hash}"
--- lesson2/work5.rb
+++ lesson2/work5.rb
@@ -5,10 +5,9 @@ month = gets.chomp.to_i
 print "Введите год "
 year = gets.chomp.to_i
 
-day_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31].sum
+day_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day_month[1] = 29 if year % 400 == 0 && (year % 4 == 0 && year % 100 == 0)
 
-number = 0
-number = day + day_month.take(month)
+number = day_month.take(month-1).sum + day
 
 puts "Порядковый номер #{number}"
